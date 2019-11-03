dad622cd4a26a7f22e7b157f288d6155a4738b49
klwaamzhang <36008792+klwaamzhang@users.noreply.github.com>
2019-11-03 15:37:43 -0500

    Can choose different Player Prefabs

---
+++ Assets/Simple Multiplayer/My Scripts/ButtonHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public NetworkCustom networkCustom;
+
+    public void setToPlayer1()
+    {
+        networkCustom.btn1();
+        Debug.Log("setToPlayer1()");
+    }
+
+    public void setToPlayer2()
+    {
+        networkCustom.btn2();
+        Debug.Log("setToPlayer2()");
+    }
+}
